ef47c4d1ae12e9ce9389d976149bbb4d7fbd8f8b
DarkImposter <psexton@cfsnc.org>
2018-06-27 01:20:55 +0000

    pi verson with funk

---
+++ piblood.py
@@ -0,0 +1,40 @@
+import time
+import random
+
+
+# __init__
+
+health = 10
+attack = 3
+running = True
+death = False
+prog = 0
+Loc = 'Nowhere, really.'
+
+
+#funk
+def c():
+    input('"Enter to continue"')
+def dead(prog):
+    running = False
+    res = input('Well you died. Try again I guess?')
+    if res == y or res == Yes or res == Y or res == yes:
+        running = True
+def stat():
+    print('your stats are:\ Health = '+str(health)+'\nLocation = '+Loc+'\nAttack = '+str(attack)+'\nLevel = '+str(prog))
+
+while running:
+    if prog == 0:
+        name = input('What is your Name?')
+        quest = input('What is your quest?')
+        color = input('What is your favorate color?')
+        print('you are ready, warrior')
+        c()
+        print('wow! welcome! you are a bloodthirsty monster itching to kill anything you find. To look for something to satisfy your need, use "hunt" or "h". \n or, to check your stats, try "s" or "stats"')
+        c()
+        t1 = input('so! lets give it a try. \n what will it be? stats, or a fight?')
+        if t1 == 'h' or 'hunt':
+            print('you have gone on a wild hunt...')
+        if t1 == 's' or 'stats':
+            stat()
+        prog += 1
